7c6a278f076e136daa2cb43768c7b748c5cbc3b4
Miguel Angel Abregu <miguel_af_555@hotmail.com>
2025-05-24 16:10:51 -0300

    creacion del componente AlumnoAgregar y array en App.jsx - Miguel Abregu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-
+import { useEffect, useState } from "react";
+import AlumnoFormulario from "./assets/components/AlumnoAgregar";
 
 function App() {
- 
-  return (
-    <div>
 
-    </div>
-  )
-}
+//creacion del array
+const [alumnos , setAlumnos] = useState([]);
 
-export default App
+//funcion flecha para agregar el objeto alumno al array
+const agregarAlumno = (alumno) => {
+ setAlumnos([...alumnos, alumno ]);
+ console.log("Alumnos:", alumnos);
+};
+
+useEffect(() => {
+    console.log("lista de alumnos" , alumnos)
+}, [alumnos]);
+
+
+return(
+    <AlumnoFormulario onAgregar={agregarAlumno} />
+);
+};
+export default App;
