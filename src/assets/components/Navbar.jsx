345a35f66558e40cad8b04fd0eb1affe90ca53fd
Miguel Angel Abregu <miguel_af_555@hotmail.com>
2025-06-02 15:46:00 -0300

    Creacion de barra de menu-navbar- Miguel Abregu

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from 'react-router-dom'; 
+
+const Menu = () => {
+    return (
+    <Navbar bg="primary" variant="dark" expand="lg" className="mb-4 py-2">
+        <Container>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link as={NavLink} to='/' >Home</Nav.Link>
+            <Nav.Link as={NavLink} to='/alumnos'>Lista de Alumnos</Nav.Link>
+            <Nav.Link as={NavLink} to='/nuevo-alumno'>Nuevo Alumno</Nav.Link>
+            <Nav.Link as={NavLink} to='/acerca-de'>Acerca de</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+
+
+    ) 
+};
+export default Menu;
